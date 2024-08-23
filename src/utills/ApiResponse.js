6263dec9c9fd867a9696160a4a81ad7a506c0227
Arun Khatri <akcs4444@gmail.com>
2024-08-23 16:08:28 +0545

    setup user and video model

--- src/utills/ApiResponse.js
+++ src/utills/ApiResponse.js
@@ -3,7 +3,7 @@
 class ApiResponse {
     consturctor(status, data, message = "Success") {
         this.status = status;
-        this.message = success;
+        this.message = message;
         this.data = data;
         this.success = status < 400
     }
